0348c26073e6612b290d0e8a37a42ef57f1c8617
MezzoDiego <diegomezzo90@gmail.com>
2022-11-28 15:29:52 +0100

    setup strato service web app

---
+++ src/main/java/it/prova/agendarest/service/AgendaServiceImpl.java
@@ -0,0 +1,69 @@
+package it.prova.agendarest.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.prova.agendarest.model.Agenda;
+import it.prova.agendarest.repository.agenda.AgendaRepository;
+
+@Service
+@Transactional(readOnly = true)
+public class AgendaServiceImpl implements AgendaService{
+
+	@Autowired
+	private AgendaRepository repository;
+
+	@Override
+	public List<Agenda> listAllElements(boolean eager) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Agenda caricaSingoloElemento(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Agenda caricaSingoloElementoEager(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@Transactional
+	public Agenda aggiorna(Agenda agendaInstance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@Transactional
+	public Agenda inserisciNuovo(Agenda agendaInstance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@Transactional
+	public void rimuovi(Long idToRemove) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Agenda> findByExample(Agenda example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Agenda> findByDescrizione(String descrizione) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
